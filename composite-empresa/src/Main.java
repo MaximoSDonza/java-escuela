6f49549e95252a2fe78edc1853930cd65a6d4dc3
DonzaEscuela <maximosebastiandonza@gmail.com>
2024-03-22 19:25:41 +0100

    Empresa terminada

--- composite-empresa/src/Main.java
+++ composite-empresa/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Departamento departamento1= new Departamento();
+        Departamento departamento2= new Departamento();
+        Empleado empleado1 = new Empleado("carlos");
+        Empleado empleado2 = new Empleado("pablo");
+        Empleado empleado3 = new Empleado("santiago");
+        departamento2.agregar(empleado2);
+        departamento2.agregar(empleado3);
+        departamento1.agregar(empleado1);
+        departamento1.agregar(departamento2);
+        departamento1.operacion();
     }
+
 }
--- composite-empresa/src/SubDepartamento.java
+++
@@ -1,5 +0,0 @@
-public class SubDepartamento implements Complemento{
-    public void operacion(){
-
-    }
-}
